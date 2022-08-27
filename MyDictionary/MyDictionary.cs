a5844a5ca4ff01fbae159c672b95ea57e281ace0
BilginTugcenur <92152213+BilginTugcenur@users.noreply.github.com>
2022-08-27 14:07:41 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿namespace MyDictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] _array;
+        TValue[] _array2;
+        TKey[] _tempArray;
+        TValue[] _tempArray2;
+        public MyDictionary()
+        {
+            _array = new TKey[0];
+            _array2 = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            _tempArray = _array;
+            _tempArray2 = _array2;
+            _array = new TKey[_array.Length+1];
+            _array2 = new TValue[_array2.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+                _array2[i] = _tempArray2[i];
+
+            }
+            _array[_array.Length-1] = key;
+            _array2[_array2.Length-1] = value;
+        }
+        
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+        public void Show()
+        {
+            Console.WriteLine("Key\tValue");
+            for (int i = 0; i < _array.Length; i++)
+            {
+                Console.WriteLine(_array[i].ToString() + "\t" + _array2[i]);
+            }
+            
+        } 
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,int> dictionary = new Dictionary<int, int>();
+            dictionary.Add(1, 10);
+            dictionary.Add(2, 20); 
+            dictionary.Add(3, 30);
+
+            MyDictionary<int,int> dictionary2 = new MyDictionary<int,int>();
+            dictionary2.Add(1,10);
+            dictionary2.Add(2, 20);
+            Console.WriteLine(dictionary2.Count);
+            dictionary2.Show();
+            
+        }
+    }
+}
